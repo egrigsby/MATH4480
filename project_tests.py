9452ccf923afb08154184ae15ec4db848691cf14
m-a-huber <hubermarius@hotmail.com>
2022-08-10 15:23:37 -0400

    Added test functions for Project 5.

--- project_tests.py
+++ project_tests.py
@@ -352,8 +352,8 @@ def test_perceptron(func):
 # PROJECT 2:
 
 def test_preprocess_sk(func):
-    x_inputs, y_inputs = np.random.uniform(1,100, (20,10)), np.random.choice([-1,1],(20,))
-    expected_outputs = np.append(x_inputs, np.ones((x_inputs.shape[0],1)), axis=1) # Append a 1 to each data point
+    x_inputs, y_inputs = np.random.uniform(1,100, (10,6)), np.random.choice([-1,1],(10,))
+    expected_outputs = np.append(x_inputs, np.ones((x_inputs.shape[0],1)), axis=1)
     expected_outputs = expected_outputs * y_inputs.reshape(-1,1)
     if (func(x_inputs, y_inputs) == expected_outputs).all():
         print(GREEN + 'Test passed')
@@ -456,6 +456,16 @@ def test_kl_divergence(func):
 ###########################################################################
 
 # PROJECT 5:
+
+def test_reduce_k_dim(func):
+    x_inputs, V_input = np.random.choice(range(100), (6,3)), np.random.choice(range(100), (3,3))
+    expected_outputs = [np.matmul(x_inputs, V_input[:,:k]) for k in range(1,4)]
+    try:
+        if min([(func(x_inputs, V_input, k) == expected_outputs[k-1]).all() for k in range(1,4)]) == 1:
+            print(GREEN + 'Test passed')
+    except:
+        print(RED + 'Test did not pass' + BLACK)
+        print(f'For the inputs: x_train = {x_inputs}, and V = {V_input}\nThe expected outputs are {expected_outputs} \nYour function outputs are {[func(x_inputs, V_input, k) for k in range(1,4)]}')
 
 ###########################################################################
 
